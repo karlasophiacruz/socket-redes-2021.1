9e6821dc506d4cd2ce673ca97ed965f9c9965b48
Karla Sophia Cruz <karlasophiacruz2001@gmail.com>
2022-02-20 14:35:52 -0300

    Código funcionalgit add .

--- src/client.py
+++ src/client.py
@@ -1,40 +1,106 @@
 #!/usr/bin/env python3
 import time
 import socket
+import threading
+
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
+c = None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data = []
+player1 = {
+    "name": "",
+    "symbol": "X",
+    "score": 0
+}
+player2 = {
+    "name": "",
+    "symbol": "O",
+    "score": 0
+}
 
-    # Mantem uma conexão com o servidor
+turno = 0
+
+def running_game_t(c, m, name):
+    global turno, player1, player2
+    data_server = " "
     while True:
-        # Recebe as mensagens do servidor que informam o Jogador, Turno, Tabuleiro e se acabou
-        while True:
-            time.sleep(2)
-            byte = s.recv(1)
-            print(byte.decode('utf-8'))
+        data_server = c.recv(4096)
 
-            if byte.decode('utf-8') == "1":
-                break
-        
-        while True:
-            byte = s.recv(1024)
-            if not byte:
-                print("entrou")
-                stre = ' '.join(map(str, data))
-                strr = stre.split('$')
-                player = strr[0]
-                turno = strr[1]
-                board = strr[2]
-                acabou = strr[3]
-                print(player)
-                print(turno)
-                print(board)
-                print(acabou)
-                break
+        if not data_server:
+            break
 
-            data.append(byte.decode('utf-8'))
+        data_server = data_server.decode('utf-8')
         
+        if data_server.startswith("welcome"):
+            if data_server == "welcome1":
+                player1["name"] = name
+                print("Sophlia: Welcome " + player1["name"] + "! Waiting for your opponent")
+            
+            elif data_server == "welcome2":
+                player2["name"] = name
+                print("Sophlia: Welcome " + player2["name"] + "! Yeah! Tic Tac Toe will start soon")
+
+        elif data_server.startswith("iamplayer"):
+            if data_server.startswith("iamplayer1$"):
+                print("SophLia: Hey! Your game will start now! \n")
+                player2["name"] = data_server[11 : len(data_server)]
+                print("SophLia: Your opponent is", player2["name"])
+            
+            elif data_server.startswith("iamplayer2$"):
+                player1["name"] = data_server[11 : len(data_server)]
+                print("SophLia: Your opponent is", player1["name"])
+        
+        
+        time.sleep(3)            
+
+def connect_server():
+    global c, HOST, PORT, player1, player2
+
+    name = input("Sophlia: Hey, player! Enter your name: ")
+    
+    while len(name) == 0:
+        name = input("Enter your name, please! ")
+
+    try:
+        c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        c.connect((HOST, PORT))
+        #data = []
+        c.send(name.encode('utf-8'))
+        
+        threading.Thread(target=running_game_t, args=(c, "m", name)).start()
+    
+    except Exception as e:
+        print("ERROR")
+
+    
+        # Mantem uma conexão com o servidor
+        # while True:
+            # Recebe as mensagens do servidor que informam o Jogador, Turno, Tabuleiro e se acabou
+            #while True:
+                # time.sleep(2)
+                # byte = s.recv(1)
+                #print(byte.decode('utf-8'))
+
+                # if byte.decode('utf-8') == "1":
+                #     break
+            
+            # while True:
+            #     # byte = s.recv(1024)
+            #     if not byte:
+            #         print("entrou")
+            #         stre = ' '.join(map(str, data))
+            #         strr = stre.split('$')
+            #         player = strr[0]
+            #         turno = strr[1]
+            #         board = strr[2]
+            #         acabou = strr[3]
+            #         print(player)
+            #         print(turno)
+            #         print(board)
+            #         print(acabou)
+            #         break
+
+            #     data.append(byte.decode('utf-8'))
+        
+connect_server()
--- src/server.py
+++ src/server.py
@@ -5,88 +5,149 @@ import time
 
 HOST = '127.0.0.1'
 PORT = 65432
+#s = None
+
 
 #Cria um tabuleiro de jogo vazio
 board = TicTacToe()
 players = []
+players_name = []
 turno = 'X'
 acabou = 0
 
-def connection_t(conn, addr):
-    global turno, acabou
+#start_server()
+
+# Função para executar o jogo e enviar as rodadas para o outro jogador
+def game_t(conn, addr):
+    global turno, acabou, players, players_name
     player = 0
-
-    if turno == 'O':
-        player = 'O'
-        turno = 'X'
-    else:
-        player = 'X'
-        turno = 'O'
+    message = ""
 
     while True:
-        if len(players) == 2:
-            conn.sendall('1'.encode('utf-8'))
-            msg = player + '$' + turno + '$\n' + str(board) + '$' + str(acabou)
-            conn.sendall(msg.encode('utf-8'))    
+    # recebe o nome do jogador
+        time.sleep(1)
+        player_name = conn.recv(1024)
+        #print(player_name)
+        if len(player_name) > 1:
             break
-        else:
-            time.sleep(1)
-            conn.sendall('0'.encode('utf-8'))
+    
+    #player_name = player_name.rstrip()
+    players_name.append(player_name.decode('utf-8'))
+    print(players_name[-1])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    while True:
-        conn, addr = s.accept()
-        if len(players) < 2:
-            with conn:
-                player = threading.Thread(target=connection_t, args=(conn, addr))
-                player.start()
-                players.append(player)
-                print('Jogador se conectou!', len(players))
+    # define os turnos de cada jogador
+    # if turno == 'O':
+    #     player = 'O'
+    #     turno = 'X'
+    # else:
+    #     player = 'X'
+    #     turno = 'O'
+
+    # envia mensagem de 'bem-vindo'
+    if len(players) < 2:
+        msg = "welcome1"
+        conn.send(msg.encode('utf-8'))
+        #print(players[0], conn)
+    else:
+        msg = "welcome2"
+        conn.send(msg.encode('utf-8'))
+        #print(players[1], conn)
+        time.sleep(1)
+
+        # envia os dados dos jogadores
+        msg = "iamplayer1$" + players_name[1]
+        players[0].send(msg.encode('utf-8'))
+
+        msg = "iamplayer2$" + players_name[0]
+        players[1].send(msg.encode('utf-8'))
         
-            
-            # data = conn.recv(1024)
-            # if not data:
-            #     break
-            # conn.sendall(data)
-        else:
-            conn.sendall('Já temos muitos jogadores, querido!'.encode('utf-8'))
-            conn.close() 
-            
-            
-            #board.printBoard()
-            
-            
-            # jogador = 0
-            # self.board = criarBoard()
-            # ganhador = verificaGanhador(self.board)
+    # while True:
+        
+    #     # recebe a jogada do jogador
+    #     data = conn.recv(4096)
+    #     if not data:
+    #         break
+        
+    #     # envia a jogada do outro jogador para o adversário
+    #     if data.startswith("$xy$"):
+    #         if conn == players[0]:
+    #             players[1].send(data)
+    #         else:
+    #             players[0].send(data) 
+    
+    # remove o jogador após o fim da partida ou perda de conexão
+    #if conn == players[0]:
+    #    del players[0]
+    #else:
+    #    del players[1]
 
-            # while(not ganhador):
-            #     printBoard(self.board)
-            #     print("=========================")
+    #conn.close()         
+                 
+                      
+def start_server():
+    global HOST, PORT
 
-            #     if(jogador == 0):
-            #     i, j = movimentoIA(self.board, jogador)
-            #     #i = getInputValido("Digite a linha: ")
-            #     #j = getInputValido("Digite a coluna: ")
-                
-            #     else:
-            #     i, j = movimentoIA(self.board, jogador)
-            #     #i = getInputValido("Digite a linha: ")
-            #     #j = getInputValido("Digite a coluna: ")
-                
-                
-            #     if(verificaMovimento(self.board, i, j)):
-            #     fazMovimento(self.board, i, j, jogador)
-            #     jogador = (jogador + 1) % 2
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
 
-            #     else:
-            #     print("A posicao informada ja esta ocupada")
+        # Aceita novos jogadores se o número de jogadores ativos for menor que 2 
+        while True:
+            conn, addr = s.accept()
+            if len(players) < 2:
+                print('Jogador se conectou!', len(players))
+                players.append(conn)
+                threading._start_new_thread(game_t, (conn, addr))
+                #player.start()
+                    
+            
                 
-            #     ganhador = verificaGanhador(self.board)
+                # data = conn.recv(1024)
+                # if not data:
+                #     break
+                # conn.sendall(data)
+            else:
+                # conn.sendall('Já temos muitos jogadores, querido!'.encode('utf-8'))
+                print('Jogador vai embora!', len(players))
+                
+                #conn.close() 
+                
+                
+                #board.printBoard()
+                
+                
+                # jogador = 0
+                # self.board = criarBoard()
+                # ganhador = verificaGanhador(self.board)
 
-            # print("=========================")
-            # printBoard(self.board)
-            # print("Ganhador = ", ganhador)
-            # print("=========================")
+                # while(not ganhador):
+                #     printBoard(self.board)
+                #     print("=========================")
+
+                #     if(jogador == 0):
+                #     i, j = movimentoIA(self.board, jogador)
+                #     #i = getInputValido("Digite a linha: ")
+                #     #j = getInputValido("Digite a coluna: ")
+                    
+                #     else:
+                #     i, j = movimentoIA(self.board, jogador)
+                #     #i = getInputValido("Digite a linha: ")
+                #     #j = getInputValido("Digite a coluna: ")
+                    
+                    
+                #     if(verificaMovimento(self.board, i, j)):
+                #     fazMovimento(self.board, i, j, jogador)
+                #     jogador = (jogador + 1) % 2
+
+                #     else:
+                #     print("A posicao informada ja esta ocupada")
+                    
+                #     ganhador = verificaGanhador(self.board)
+
+                # print("=========================")
+                # printBoard(self.board)
+                # print("Ganhador = ", ganhador)
+                # print("=========================")
+
+
+start_server()
